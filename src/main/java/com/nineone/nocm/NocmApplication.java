21decb5f14a53b3e47c6107d24ae01e3b7f360e9
leejuhyeok <tyrfingr12@naver.com>
2020-02-20 15:44:16 +0900

    add All

---
+++ src/main/java/com/nineone/nocm/NocmApplication.java
@@ -0,0 +1,13 @@
+package com.nineone.nocm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NocmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NocmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nineone/nocm/NocmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nineone.nocm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NocmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
