044fac1c93474d8654d9c0aab114dd94da0e4ad8
leejuhyeok <tyrfingr12@naver.com>
2020-02-21 16:21:21 +0900

    시큐리티 부분 생성

---
+++ src/main/java/com/nineone/nocm/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.nineone.nocm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+
+        filter.setForceEncoding(true);
+        http
+                .authorizeRequests()
+                .antMatchers("/")
+                .addFilter(filter, CsrfFilter.class)
+
+    }
+}
