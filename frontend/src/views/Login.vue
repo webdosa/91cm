befdc82c2e194da2c14756e6e5cbd5caab9cf46c
leejuhyeok <tyrfingr12@naver.com>
2020-02-21 15:41:32 +0900

    로그인 View 추가

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="float-left d-inline-block">
+      <img src="../assets/images/laptop.jpg" class="img-fluid float-left list-inline" alt="Responsive image">
+    </div>
+    <div class="d-inline-block" id="login-box">
+      <img src="../assets/images/nineonesoft_logo.png" class="d-inline-block img-thumbnail">
+      <form id="login-form">
+        <input id="userId" name="userId" type="text" class="form-control" placeholder="아이디를 입력해주세요">
+        <input id="pwd" name="pwd" type="password" class="form-control" placeholder="비밀번호를 입력해주세요">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="id_save">
+          <label class="form-check-label text-left" for="id_save">
+            Remember me
+          </label>
+        </div>
+        <div class="btn btn-outline-primary btn-block" onclick="window.location.href='/'">로그인</div>
+        <div class="btn btn-outline-secondary btn-block" onclick="window.location.href='/'">회원가입</div>
+      </form>
+      <br><br>
+      <p class="h6">소셜로그인</p>
+      <hr>
+      <div>
+        <a href="/"><img class="social-logo" src="../assets/images/github_logo.png"></a>
+        <a href="/"><img class="social-logo" src="../assets/images/google_logo.png"></a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+<style scoped>
+  @import "~bootstrap/dist/css/bootstrap.min.css";
+  @import '~bootstrap-vue/dist/bootstrap-vue.min.css';
+  @import "../assets/font/iconmonstr/css/iconmonstr-iconic-font.min.css";
+
+  .h6{
+    color: darkgray;
+  }
+
+  .img-fluid {
+    height: 100vh;
+    width: 70vw;
+  }
+
+  #login-box{
+    width: 28vw;
+    padding: 1.5vw;
+    margin-top: 10%;
+  }
+
+  #login-form{
+    margin-top: 20%;
+  }
+</style>
