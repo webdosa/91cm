7ce26c6ec9f61f3503415da0a70a2440616f8cd2
leejuhyeok <tyrfingr12@naver.com>
2020-02-21 10:47:10 +0900

    router catch all 설정

--- src/main/java/com/nineone/nocm/controller/CommonController.java
+++ src/main/java/com/nineone/nocm/controller/CommonController.java
@@ -1,44 +1,44 @@
-package com.nineone.nocm.controller;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-
-@Controller
-@Slf4j
-public class CommonController implements ErrorController {
-
-    @RequestMapping("/error")
-    public String errorHandler(HttpServletRequest request){
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-
-        if (status != null){
-            HttpStatus statusCode = HttpStatus.valueOf(Integer.valueOf(status.toString()));
-            log.error("Request error : "+status.toString());
-            switch (statusCode){
-                case NOT_FOUND:
-                    return "error/404";
-                case INTERNAL_SERVER_ERROR:
-                    return "error/500";
-                case FORBIDDEN:
-                    return "error/403";
-                case METHOD_NOT_ALLOWED:
-                    return "error/405";
-                default:
-                    return "error/error";
-            }
-        }
-        return "error/error";
-    }
-
-    @Override
-    public String getErrorPath() {
-        return "/error";
-    }
-}
+//package com.nineone.nocm.controller;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.boot.web.servlet.error.ErrorController;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.http.HttpServletRequest;
+//
+//@Controller
+//@Slf4j
+//public class CommonController implements ErrorController {
+//
+//    @RequestMapping("/error")
+//    public String errorHandler(HttpServletRequest request){
+//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+//
+//        if (status != null){
+//            HttpStatus statusCode = HttpStatus.valueOf(Integer.valueOf(status.toString()));
+//            log.error("Request error : "+status.toString());
+//            switch (statusCode){
+//                case NOT_FOUND:
+//                    return "forward:/";
+//                case INTERNAL_SERVER_ERROR:
+//                    return "error/500";
+//                case FORBIDDEN:
+//                    return "error/403";
+//                case METHOD_NOT_ALLOWED:
+//                    return "error/405";
+//                default:
+//                    return "error/error";
+//            }
+//        }
+//        return "error/error";
+//    }
+//
+//    @Override
+//    public String getErrorPath() {
+//        return "/error";
+//    }
+//}
---
+++ src/main/java/com/nineone/nocm/controller/RouterController.java
@@ -0,0 +1,13 @@
+package com.nineone.nocm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RouterController {
+
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect(){
+        return "forward:/";
+    }
+}
