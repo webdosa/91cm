8475b7634911b9bba0880044489633086633c4e1
leejuhyeok <tyrfingr12@naver.com>
2020-02-21 10:47:50 +0900

    404 페이지 추가

---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="notfound">
+    <div class="notfound">
+      <div class="notfound-404">
+        <h3>Page not found</h3>
+        <h1><span>4</span><span>0</span><span>4</span></h1>
+      </div>
+      <h2>해당 페이지를 찾을 수 없습니다.</h2>
+      <a href="/" class="btn btn-white btn-animation-1">GO HOME</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  @import "../assets/css/error.css";
+</style>
