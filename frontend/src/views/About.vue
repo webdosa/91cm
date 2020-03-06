680fe89f75196e8d7c63f8bd3ca2e13966d881b6
jny9708 <jny9708@gmail.com>
2020-03-06 15:19:52 +0900

    websocket

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,24 +1,79 @@
 <template>
   <div class='about'>
-    <h1>This is an about page {{info}}</h1>
+    <h1>This is an about page!!</h1>
+    <button @click="send">전송</button>
+    <button @click="check">확인</button>
   </div>
 </template>
 <script>
-import axios from 'axios'
+/* import axios from 'axios' */
+// import WebSocket from '../service/websocket'
+import SockJS from 'sockjs-client'
+import Stomp from 'webstomp-client'
+let channelList = [1, 2]
+channelList.push(3)
 export default {
   name: 'About',
   data () {
     return {
-      info: null
+      channelList: null,
+      message: {
+        channel_id: 3,
+        content: 'hihi',
+        sender: 'userid'
+      },
+      array: [],
+      currentChannel: 0,
+      msgCountObj: {}
     }
   },
-  mounted () {
-    axios.get('http://localhost:9191/api/test')
-      .then(res => {
-        this.info = res.data
-      }).catch(error => {
-        console.log(error)
+  created () {
+    this.channelList = channelList
+  },
+  methods: {
+
+    send: function() {
+      if (this.stompClient && this.stompClient.connected) {
+        this.stompClient.send("/pub/chat/message", JSON.stringify(this.message),{})
+      }
+     },
+    connect() {
+      this.stompClient = Stomp.over(new SockJS('http://localhost:9191/endpoint/'))
+      this.stompClient.connect({},() => {
+        console.log('연결')
+
+        for(let i in channelList){
+        this.stompClient.subscribe("/sub/chat/room/"+channelList[i],(e)=>{
+          let data = JSON.parse(e.body);
+          
+          if(data.channel_id == this.currentChannel){
+            this.array.push(data)  
+          }else{
+            this.msgCountObj[data.channel_id] += 1
+          }
+          
+        })
+      }
       })
+    },
+    check() {
+      console.log(this.array)
+      console.log(this.msgCountObj)
+    }
+
+    },
+    mounted () {
+      /* axios.get('http://localhost:9191/api/test')
+        .then(res => {
+          this.info = res.data
+        }).catch(error => {
+        }) */
+      this.currentChannel = 1
+      for(let i in this.channelList){
+        this.msgCountObj[this.channelList[i]] = 0
+      }
+      this.connect()
   }
 }
+
 </script>
--- src/main/java/com/nineone/nocm/controller/api/UserApiController.java
+++ src/main/java/com/nineone/nocm/controller/api/UserApiController.java
@@ -1,6 +1,11 @@
 package com.nineone.nocm.controller.api;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,5 +43,18 @@ public class UserApiController {
     public boolean idcheck(@RequestParam String userid) {
     	return userService.idcheck(userid);
     }
+    
+    @RequestMapping("/user")
+    public List<Integer> getchannel() {
+    	
+    	List<Integer> arr = new ArrayList<>();
+    	arr.add(1);
+    	arr.add(2);
+    	arr.add(3);
+    	
+    	return arr;
+    }
+    
+    
     
 }
