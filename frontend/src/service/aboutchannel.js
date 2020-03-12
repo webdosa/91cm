4c00953e51270bcdb7c2190a65df36392e019aaa
leejuhyeok <tyrfingr12@naver.com>
2020-03-12 13:11:03 +0900

    api 주소 변경

--- frontend/src/service/aboutchannel.js
+++ frontend/src/service/aboutchannel.js
@@ -3,7 +3,7 @@ class AboutChannel{
     // 임시로 제가 만든 채널리스트 가져오는 api입니다.
     // 추후에 코드 합치고나서 만들어주신 api 쓸 예정입니다.
     getChannelList () {
-        return axios.get('http://localhost:9191/api/user')
+        return axios.get('http://localhost:9191/api/channel/list')
     }
     getMsgList (){
         return axios.get('')
