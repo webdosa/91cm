311d8810bd83c109f05e5d6518b6182372523882
leejuhyeok <tyrfingr12@naver.com>
2020-03-13 12:28:32 +0900

    DB user 변경

--- src/main/java/com/nineone/nocm/config/DataSourceConfig.java
+++ src/main/java/com/nineone/nocm/config/DataSourceConfig.java
@@ -27,8 +27,8 @@ public class DataSourceConfig {
         config.setMinimumIdle(5);
         config.setDriverClassName("com.mysql.cj.jdbc.Driver");
         config.setJdbcUrl("jdbc:mysql://localhost:3306/nocm?serverTimezone=Asia/Seoul&characterEncoding=UTF-8&verifyServerCertificate=false&useSSL=false");
-        config.addDataSourceProperty("user", "hkoo");
-        config.addDataSourceProperty("password", "dlwngur12");
+        config.addDataSourceProperty("user", "91cm");
+        config.addDataSourceProperty("password", "91cm");
 		return config;
 	}
 	
