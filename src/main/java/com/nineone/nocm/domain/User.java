0e35ae08000c03cf3a80e6e27175d01005035a05
lee <tyrfingr12@naver.com>
2020-05-06 17:51:06 +0900

    gradlew 권한 변경

--- src/main/java/com/nineone/nocm/domain/User.java
+++ src/main/java/com/nineone/nocm/domain/User.java
@@ -5,22 +5,28 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Getter
 @NoArgsConstructor
 @Setter
-public class User {
+public class User implements UserDetails {
     private String name;
     private String phone;
     private String email;
     private String picture;
+    private String password;
 
     @Builder
-    public User(String name, String phone, String picture, String email) {
+    public User(String name, String phone, String picture, String email, String password) {
         this.name = name;
         this.phone = phone;
         this.picture = picture; 
         this.email = email;
+        this.password = password;
         
     }
 
@@ -28,5 +34,41 @@ public class User {
         this.name = name;
         this.picture = icon;
         return this;
+    }
+
+    //Form 로그인 설정
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
