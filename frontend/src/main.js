ff486b59473e6e4f5eb2385d9aef5c51f36146f6
lee <tyrfingr12@naver.com>
2020-05-15 16:41:05 +0900

    이름 버그 수정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,7 +12,7 @@ import "@/assets/css/app.scss"
 import AlertModal from "./plugins/AlertModal";
 import './assets/font/iconmonstr/css/iconmonstr-iconic-font.min.css';
 import 'animate.css'
-import vuetify from 'vuetify'
+import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import './assets/css/main.css'
 
@@ -23,7 +23,7 @@ Date.prototype.addDays = function (days) {
   return date;
 }
 Vue.use(AlertModal)
-Vue.use(vuetify)
+Vue.use(Vuetify)
 Vue.use(VueSession, { persist: true})
 Vue.prototype.$eventBus = new Vue();
 Vue.prototype.$http = axios
