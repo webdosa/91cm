17b143fb19d3072dc298824ac24cadea9636ffcd
lee <tyrfingr12@naver.com>
2020-06-02 13:19:04 +0900

    RTC 모듈 추가

--- frontend/src/views/util/DevelopView.vue
+++ frontend/src/views/util/DevelopView.vue
@@ -4,7 +4,7 @@
     <p style="text-align: center" class="h4 animate__animated animate__fadeIn animate__delay-1s">개발자가 카페인을 분해하여 코드로 배출중입니다. 개발을 기다려주세요.</p>
     <b-img class="animate__animated animate__backInLeft" src="https://image.freepik.com/free-vector/programmers-concept-with-flat-design_23-2147862433.jpg"
            fluid center></b-img>
-    <b-button size="lg" @mouseover="addAnimation">GO MAIN PAGE</b-button>
+    <b-button size="lg">GO MAIN PAGE</b-button>
   </div>
 </template>
 
@@ -12,11 +12,7 @@
   export default {
     name: "DevelopView",
     methods:{
-      addAnimation:function (e) {
-        console.log(e.target.)
-        // e.target.toggle('animate__animated')
-        // e.target.toggle('animate__bounceInDown')
-      }
+
     }
   }
 </script>
