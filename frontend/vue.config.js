df9941819e88c25a8dfb32e9baf87a8ed06cc1b8
lee <tyrfingr12@naver.com>
2020-06-02 16:43:09 +0900

    vue 프록시 설정 변경

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,35 +4,35 @@ module.exports = {
   lintOnSave: false,
   // productionSourceMap: false,
   outputDir: path.resolve(__dirname, '../' + 'src/main/resources/static'),
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:9191',
-        // ws: false,
-        changeOrigin: true,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-        }
-      },
-      '/oauth2':{
-        target: 'http://localhost:9191',
-        changeOrigin: true,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-        }
-      },
-      '/endpoint':{
-        target: 'http://localhost:9191',
-        changeOrigin: true,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-        }
-      }
-    }
-  },
+  // devServer: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:9191',
+  //       // ws: false,
+  //       changeOrigin: true,
+  //       headers: {
+  //         'Access-Control-Allow-Origin': '*',
+  //         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+  //       }
+  //     },
+  //     '/oauth2':{
+  //       target: 'http://localhost:9191',
+  //       changeOrigin: true,
+  //       headers: {
+  //         'Access-Control-Allow-Origin': '*',
+  //         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+  //       }
+  //     },
+  //     '/endpoint':{
+  //       target: 'http://localhost:9191',
+  //       changeOrigin: true,
+  //       headers: {
+  //         'Access-Control-Allow-Origin': '*',
+  //         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+  //       }
+  //     }
+  //   }
+  // },
   configureWebpack: {
     entry: ['babel-polyfill', './src/main.js']
   }
